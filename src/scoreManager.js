b6746ad8b1072ade04226a8b60ca6bbd5a7714bb
ivan.dimitrov <ivan.dimitrov@fit.fraunhofer.de>
2024-11-07 10:31:39 +0100

    code updated

--- src/scoreManager.js
+++ src/scoreManager.js
@@ -1,20 +1,38 @@
 import axios from 'axios';
 
 // Request nonce from server
+// export const requestNonce = async (wallet) => {
+//     try {
+//         //const response = await axios.post('https://server.thedrop.top/api/generateNonce', { wallet });
+//         const response = await axios.post('http://localhost:5001/api/generateNonce', { wallet });
+//         const { nonce } = response.data;
+//         localStorage.setItem('gameNonce', nonce);  // Store the nonce locally
+//     } catch (error) {
+//         console.error('Error generating nonce:', error);
+//     }
+// };
+
 export const requestNonce = async (wallet) => {
     try {
-        const response = await axios.post('https://server.thedrop.top/api/generateNonce', { wallet });
+        if (!wallet) {
+            console.error('Wallet address is missing');
+            return;
+        }
+        console.log('Requesting nonce for wallet:', wallet);
+        const response = await axios.post('http://localhost:5001/api/generateNonce', { wallet });
         const { nonce } = response.data;
-        localStorage.setItem('gameNonce', nonce);  // Store the nonce locally
+        localStorage.setItem('gameNonce', nonce);
     } catch (error) {
         console.error('Error generating nonce:', error);
     }
 };
 
+
 export const storePoints = async (wallet) => {
     try {
         const nonce = localStorage.getItem('gameNonce');
-        const response = await axios.post('https://server.thedrop.top/api/savePoints', {
+       // const response = await axios.post('https://server.thedrop.top/api/savePoints', {
+        const response = await axios.post('http://localhost:5001/api/savePoints', {
             wallet,
             nonce
         });
